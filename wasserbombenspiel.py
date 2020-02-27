138dbf9f6e9ec14c5afdc64281d576c6133832f5
Stefan May <stefan.may@th-nuernberg.de>
2020-02-27 12:19:52 +0100

    Erste Version mit beweglichen Actor

--- wasserbombenspiel.py
+++ wasserbombenspiel.py
@@ -1,11 +1,30 @@
 import pgzrun
 import random
 
+WIDTH  = 1280
+HEIGHT = 960
+SPEED  = 2
+
+spieler         = Actor("spieler")
+spieler.pos     = WIDTH/2, HEIGHT/2
 wasserbombe_rot = Actor("wasserbombe_rot")
 
 # Das ist die Funktion zum Zeichnen
 def draw():
-    screen.clear()
-    wasserbombe_rot.draw()
+    screen.fill((0, 255, 0))
+    spieler.draw()
+
+def update():
+    if keyboard.left:
+        spieler.x -= SPEED
+
+    if keyboard.right:
+        spieler.x += SPEED
+
+    if keyboard.up:
+        spieler.y -= SPEED
+
+    if keyboard.down:
+        spieler.y += SPEED
 
 pgzrun.go()
